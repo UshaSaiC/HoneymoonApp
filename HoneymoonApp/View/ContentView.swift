994eebf46feac1eb80a21514b0075c7018bca254
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-24 15:55:55 +0530

    removing the top card and updating the last card index so that we can see one card after other on disposal

--- HoneymoonApp/View/ContentView.swift
+++ HoneymoonApp/View/ContentView.swift
@@ -12,19 +12,31 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
+    @State private var lastCardIndex: Int = 1
     
     private var dragAreaThreshold: CGFloat = 65.0
     
     @GestureState private var dragState = DragState.inactive
     
-    var cardViews: [CardView] = {
+    @State var cardViews: [CardView] = {
         var views = [CardView]()
-        // rendering only 2 cards to reduce the load time as ideally we need one card to be displayed and one card to be coming up when the top one is removed
         for index in 0..<2 {
             views.append(CardView(honeymoon: honeymoonData[index]))
         }
         return views
     }()
+    
+    private func moveCards() {
+        cardViews.removeFirst()
+        
+        self.lastCardIndex += 1
+        
+        let honeymoon = honeymoonData[lastCardIndex % honeymoonData.count]
+        
+        let newCardView = CardView(honeymoon: honeymoon)
+        
+        cardViews.append(newCardView)
+    }
     
     private func isTopCard(cardView: CardView) -> Bool {
         guard let index = cardViews.firstIndex(where: { $0.id == cardView.id }) else {
@@ -34,9 +46,6 @@ struct ContentView: View {
     }
     
     enum DragState {
-        // inactive state basically means no action on UI by user
-        // pressing state is when user taps on screen
-        // dragging state is when user drags the cards or moves the finger on UI around
         case inactive
         case pressing
         case dragging(translation: CGSize)
@@ -81,16 +90,16 @@ struct ContentView: View {
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
                         .overlay(
-                          ZStack {
-                            Image(systemName: "x.circle")
-                              .modifier(SymbolModifier())
-                              .opacity(self.dragState.translation.width < -self.dragAreaThreshold && self.isTopCard(cardView: cardView) ? 1.0 : 0.0)
-                            
-                            Image(systemName: "heart.circle")
-                              .modifier(SymbolModifier())
-                              .opacity(self.dragState.translation.width > self.dragAreaThreshold && self.isTopCard(cardView: cardView) ? 1.0 : 0.0)
-                          }
-                      )
+                            ZStack {
+                                Image(systemName: "x.circle")
+                                    .modifier(SymbolModifier())
+                                    .opacity(self.dragState.translation.width < -self.dragAreaThreshold && self.isTopCard(cardView: cardView) ? 1.0 : 0.0)
+                                
+                                Image(systemName: "heart.circle")
+                                    .modifier(SymbolModifier())
+                                    .opacity(self.dragState.translation.width > self.dragAreaThreshold && self.isTopCard(cardView: cardView) ? 1.0 : 0.0)
+                            }
+                        )
                         .offset(x: self.isTopCard(cardView: cardView) ?  self.dragState.translation.width : 0, y: self.isTopCard(cardView: cardView) ?  self.dragState.translation.height : 0)
                         .scaleEffect(self.dragState.isDragging && self.isTopCard(cardView: cardView) ? 0.85 : 1.0)
                         .rotationEffect(Angle(degrees: self.isTopCard(cardView: cardView) ? Double(self.dragState.translation.width / 12) : 0))
@@ -107,8 +116,15 @@ struct ContentView: View {
                                     break
                                 }
                             })
-                        )
-                }
+                                .onEnded({ (value) in
+                                    guard case .second(true, let drag?) = value else {
+                                        return
+                                    }
+                                    if drag.translation.width < -self.dragAreaThreshold || drag.translation.width > self.dragAreaThreshold {
+                                        self.moveCards()
+                                    }
+                                })
+                        )}
             }
             .padding(.horizontal)
             
