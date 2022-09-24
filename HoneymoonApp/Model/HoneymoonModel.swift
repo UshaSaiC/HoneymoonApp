f5c5a2d748a18c90a3a3ebdcfd3fc007826d4ef7
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-24 11:52:18 +0530

    Added card view to main screen

---
+++ HoneymoonApp/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  HoneymoonApp
+//
+//  Created by Usha Sai Chintha on 24/09/22.
+//
+
+import SwiftUI
+
+struct Destination {
+  var place: String
+  var country: String
+  var image: String
+}
