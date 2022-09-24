eefde40b5e72858e4792759fd0eba07381ef8eb6
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-24 10:15:34 +0530

    Initial Commit

---
+++ HoneymoonApp/HoneymoonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonAppApp.swift
+//  HoneymoonApp
+//
+//  Created by Usha Sai Chintha on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
